cf18cbcec8507e0c0b595313b4dc00281653050e
Gambit90s <44451389+Gambit90s@users.noreply.github.com>
2019-01-24 12:37:47 -0700

    3rd update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,30 @@
-//placeholder
+document.addEventListener("DOMContentLoaded", button);
+function button() {
+    let button = document.createElement("button");
+    let btnText = document.createTextNode("Add Square!");
+    button.className = "button";
+    button.appendChild(btnText);
+    document.body.appendChild(button);
+    let id = 0;
+
+
+    button.addEventListener("click", buttonClick);
+    function buttonClick() {
+
+        id++;
+
+        let square = document.createElement("div");
+        square.id = id;
+
+        square.className = "black-square";
+
+        let p = document.createElement("p");
+        let squareText = document.createTextNode(id);
+        p.appendChild(squareText);
+        square.appendChild(p);
+        document.body.appendChild(square);
+
+
+
+    }
+}
